3f93ea8af7c6da4bf9d1b0e80298b6ebb6b4e3e3
Luiz Guilherme Martinho Sampaio Ito <41489663+Guiimartinho@users.noreply.github.com>
2018-07-27 16:56:05 -0300

    Update Funcao_SATD.vhd

--- Funcao_SATD.vhd
+++ Funcao_SATD.vhd
@@ -1,91 +1,93 @@
-----------------------------------------------------------------
-----------------------------------------------------------------
--- 				Universidade Federal de Pelotas					  --
---																				  --
---						Engenharia Eletrônica							  --
---																				  --
---						 Sistemas Digitais I								  --
---																				  --
--- Projeto função SATD (Sum of Absolute Transform Difference) --
--- -(Soma das Diferenças Transfromadas Absolutas), utilizando --
--- a Transformada de Hadamard na forma de circuito sequencial --
--- 																			  --
--- Autores: Luiz Guilherme Martinho Sampaio Ito					  --
---				Marcelo da Costa Lopes									  --
---																				  --	
---																				  --
--- Data: Julho 2018 														  --
---																				  --
-----------------------------------------------------------------
-----------------------------------------------------------------
-
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity Funcao_SATD is
 
  port (
-		Y11, Y12, Y21, Y22 : in std_logic_vector (3 downto 0)
+		Y11, Y12, Y21, Y22 : in std_logic_vector (3 downto 0);
+		SaidaFinal         : out std_logic_vector(5 downto 0)  
 		); -- entradas do bloco
 		
 		
 end entity;
 architecture comportamento of Funcao_SATD is
 
+ --component maqui -- Maquina de estados
+--port( reset, clk	: in	std_logic;
+	--		x		: 	in std_logic;
+		--	y		:  in std_logic;
+			--q0		: out std_logic;
+			--q1		: out std_logic 
+		--); 
+
 component Reg_inicial -- Registrador de Start.
-		port (Regin0, Regin1, Regin2, Regin3, 		: in std_logic_vector;
-				Regout0, Regout1, Regout2, Regout3  : out std_logic;
-				Load_ini										: in std_logic);	
+		port (Regin0, Regin1, Regin2, Regin3, 		: in std_logic_vector (4 downto 0);
+				Regout0, Regout1, Regout2, Regout3  : out std_logic_vector (4 downto 0);
+				Load_ini										: in std_logic;	
+				Clk                                 : in std_logic
+				);
+end component
 
 component contador -- Contador do valor absoluto.
-		port (X0 , X1, X2, X3 		  : in std_logic_vector ( 5 downto 0 );
-				ABS0, ABS1, ABS2, ABS3 : out std_logic_vector ( 5 downto 0 )
+		port (
+				ABSS 						  : out std_logic_vector ( 5 downto 0 );
+				creset 					  : in std_logic;	
+				cload 					  : in std_logic;	
+				X 					        : in std_logic_vector ( 5 downto 0);
+				Clk                    : in std_logic				
 				);
 end component
 
 component MUX 	
-		port (M0, M1 , M2, M3 : in std_logic_vector (5 downto 0);
-				Saida_mux 		 : out std_logic_vector (5 downto 0 );
-				Sel_mux 			 : in std_logic_vector (1 downto 0)
+		port (M0, M1 , M2, M3          : in std_logic_vector (5 downto 0);
+				Saida_mux 		 			 : out std_logic_vector (5 downto 0 );
+				SelMux0, SelMux1			 : in std_logic_vector (1 downto 0)
 				);
 end component
 
 component Registrador1
-		port (R1 : in std_logic_vector ( 5 downto 0);
-				Q1 : out std_logic_vector (5 downto 0);
-				Load1 : in std_logic
+		port (Reg1in   : in std_logic_vector ( 5 downto 0);
+				Reg1out  : out std_logic_vector (5 downto 0);
+				Load1    : in std_logic;
+				Clk      : in std_logic;
+				Clr      : in std_logic
 				);				
 				
 end component
 
 component somador 
 		port (SomaA, SomaB 	: in std_logic_vector (5 downto 0);
-				carryin 			: in std_logic;
 				carryout 		: out std_logic;
 				saidasoma 		: out std_logic_vector (5 downto 0)
 				);
-end component				
-
-component Registrador2
-		port (R2 	: in std_logic_vector ( 5 downto 0);
-				Q2 	: out std_logic_vector (5 downto 0);
-				Load2 : in std_logic
-				);		
 end component	
 
+component Registrador2
+		port (Reg2in   : in std_logic_vector ( 5 downto 0);
+				Reg2out  : out std_logic_vector (5 downto 0);
+				Load2    : in std_logic;
+				Clk      : in std_logic;
+				Clr      : in std_logic
+				);				
+end component
+
 signal
- Re0,Re1,Re2,Re3 											 : std_logic_vector (4 downto 0);  -- signal de saida do registrador.
+-- Signal da maquinas de estados.
+
+-- Signal do codigo principal.
+ Saida_total 												 : std_logic_vector (5 downto 0);
+ --Re0,Re1,Re2,Re3 											 : std_logic_vector (4 downto 0);  -- signal de saida do registrador.
  A, A_temp, B, B_temp, C, C_temp, D, D_temp      : std_logic_vector (4 downto 0);  -- signal de entrada da primeira operação.
  W0, W1, W2, W3  : std_logic_vector (5 downto 0);  -- signal de entrada da segunda operação.
  I0, I1, I2, I3  : std_logic_vector (5 downto 0); 	-- signal de entrada do contador.
  J0, J1, J2, J3  : std_logic_vector (5 downto 0);	-- signal de entrada do mux.
- K               : std_logic_vector (5 downto 0); 	-- signal de entrada serial do registrador 1.
- O					  : std_logic_vector (5 downto 0); 	-- signal de entrada do somador.
- P					  : std_logic_vector (5 downto 0); 	-- signal de entrada do registrador 2.
- Z					  : std_logic_vector (5 downto 0);  -- signal de realimentaçao do somador.
- Load_ini		  : std_logic;								-- signal para MSS do resgistrador inicial.
- Load1			  : std_logic;								-- signal para MSS do resgistrador 1.
- Load2			  : std_logic; 							-- signal para MSS do resgistrador 2.
+ K               : std_logic_vector (5 downto 0); 	-- signal de entrada do SomaA
+ O					  : std_logic_vector (5 downto 0); 	-- signal de entrada do registrador 1
+ P					  : std_logic_vector (5 downto 0); 	-- 
+ --Z					  : std_logic_vector (5 downto 0);  -- 
+-- Load_ini		  : std_logic;								-- 
+ --Load1			  : std_logic;								--.
+ --Load2			  : std_logic; 							-- .
  
  
 begin
@@ -116,10 +118,10 @@ begin
 	
 	
 	-- Ligaçao Start.
-	Start : reg_inicial port map (Reg_entrada => Y11 , Reg_saida => Re0, Clk => Clk
-	Start : reg_inicial port map (Reg_entrada => Y12 , Reg_saida => Re1, Clk => Clk
-	Start : reg_inicial port map (Reg_entrada => Y21 , Reg_saida => Re3, Clk => Clk
-	Start : reg_inicial port map (Reg_entrada => Y22 , Reg_saida => Re4, Clk => CLk
+	Start : reg_inicial port map (Reg_entrada => Y11 , Reg_saida => Re0, Clk => Clk);
+	Start : reg_inicial port map (Reg_entrada => Y12 , Reg_saida => Re1, Clk => Clk);
+	Start : reg_inicial port map (Reg_entrada => Y21 , Reg_saida => Re3, Clk => Clk);
+	Start : reg_inicial port map (Reg_entrada => Y22 , Reg_saida => Re4, Clk => CLk);
 
 
 	-- Ligação contador absoluto com mux 4x1.
@@ -128,20 +130,24 @@ begin
 	ContadorABS : contador port map ( X2 => I2,	ABS2 => J2, Clk => Clk);
 	ContadorABS : contador port map ( X3 => I3,	ABS3 => J4, Clk => Clk);
 	
-	--Ligação mux 4x1 com registrador 1.	
+	--Ligação saida mux com somador.	
 	Mux4x1 : Mux port map ( M0 => J0,	Saida_mux => K, Clk => Clk); 
         Mux4x1 : Mux port map ( M1 => J1,	Saida_mux => K, Clk => Clk); 
 	Mux4x1 : Mux port map ( M2 => J2,	Saida_mux => K, Clk => Clk); 
 	Mux4x1 : Mux port map ( M3 => J3,	Saida_mux => K, Clk => Clk); 
-
-	-- Ligação do registrador1 com somador.
-	Reg1 : Registrador1 port map ( R1 => K,	Q1 => O, Clk => Clk); 
 	
+	-- Ligaçao somador com registrador 1.
 	
-	-- Ligaçao do somador com registrador 2.
-	Soma : somador port map ( SomaA => O, saidasoma => P ); 
-	Soma : Registrador1 port map ( SomaB => O, saidasoma => P ); 
-
-	-- Ligaçao registrador realimentaçao
-	Reg2 : Registrador2 port map (R2 => P, Q2 => Z , Clk => Clk);
+	Soma : Somador port map (SomaA => K, Saidasoma => O , Clk => Clk);
+   Soma : Somador port map (SomaB => P, Saidasoma => O , Clk => Clk);
+	
+	-- Ligaçao registrador 1 com registrador 2.
+	
+	Reg1 : Registrador1 port map (Reg1in => O , Reg1out => P, Clk => Clk);	
+	
+	-- Saida registrador 2.
+	
+	Reg2 : Registrador2 port map (Reg2in => P , Reg2out => SaidaFinal, Clk => Clk);
+	
+end architecture;	
 	
